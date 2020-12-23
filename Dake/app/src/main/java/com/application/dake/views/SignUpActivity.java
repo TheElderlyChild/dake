334c7d271f5c1872ae400ba9d4386948a1069df0
TheElderlyChild <carlevearistegermain@gmail.com>
2020-12-22 19:06:43 -0500

    add navigation between sign up and login pages

--- Dake/app/src/main/java/com/application/dake/views/SignUpActivity.java
+++ Dake/app/src/main/java/com/application/dake/views/SignUpActivity.java
@@ -2,7 +2,9 @@ package com.application.dake.views;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import com.application.dake.R;
 
@@ -12,5 +14,15 @@ public class SignUpActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+    }
+
+    public void gotoSignUpDetails(View view){
+        Intent signUpDetailsIntent = new Intent(this, SignUpDetails.class);
+        startActivity(signUpDetailsIntent);
+    }
+
+    public void gotoLogin(View view){
+        Intent LoginActivityIntent = new Intent(this, LoginActivity.class);
+        startActivity(LoginActivityIntent);
     }
 }
--- Dake/app/src/main/java/com/application/dake/views/SplashScreen.java
+++ Dake/app/src/main/java/com/application/dake/views/SplashScreen.java
@@ -2,15 +2,23 @@ package com.application.dake.views;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import com.application.dake.R;
 
 public class SplashScreen extends AppCompatActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
         setContentView(R.layout.screen_splash);
+        gotoSignUp();
     }
+
+    public void gotoSignUp(){
+        Intent signUpIntent = new Intent(this, SignUpActivity.class);
+        startActivity(signUpIntent);
+    }
+
 }

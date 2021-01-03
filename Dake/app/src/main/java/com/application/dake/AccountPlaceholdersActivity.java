990b8ec16cf93f8df88edfbb0b20cdfbe1122b92
OlaAina <laoluaina2000@gmail.com>
2021-01-03 15:46:11 +0100

    did some layouts

---
+++ Dake/app/src/main/java/com/application/dake/AccountPlaceholdersActivity.java
@@ -0,0 +1,14 @@
+package com.application.dake;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AccountPlaceholdersActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_account_placeholders);
+    }
+}
